b0e2ef1d5362d7a3aa1a2edcccc3823993151664
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-03-27 17:06:37 -0300

    testing the buy button

--- src/__tests__/components/Product.test.js
+++ src/__tests__/components/Product.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import ProviderMock from '../../__mocks__/ProviderMock';
-import PorductMock from '../../__mocks__/ProductMock';
+import ProductMock from '../../__mocks__/ProductMock';
 import Product from '../../components/Product';
-import { experiments } from 'webpack';
 
 describe('<Product />', () => {
   test('component renderig', () => {
@@ -13,5 +12,15 @@ describe('<Product />', () => {
       </ProviderMock>
     );
     expect(product.length).toEqual(1);
+  });
+  test('buy button', () => {
+    const handleAddToCart = jest.fn();
+    const wrapper = mount(
+      <ProviderMock>
+        <Product product={ProductMock} handleAddToCart={handleAddToCart} />
+      </ProviderMock>
+    );
+    wrapper.find('button').simulate('click');
+    expect(handleAddToCart).toHaveBeenCalledTimes(1);
   });
 });
