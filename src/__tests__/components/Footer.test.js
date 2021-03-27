d687d2cd52704aab24e9d0b3eeff35ffa39f9be8
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-03-27 16:29:31 -0300

    testing renders

--- src/__tests__/components/Footer.test.js
+++ src/__tests__/components/Footer.test.js
@@ -3,8 +3,11 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
-    test('render', () => {
-        const footer = mount(<Footer />);
+    const footer = mount(<Footer />);
+    test('component rendering', () => {
         expect(footer.length).toEqual(1);
     });
+    test('title rendering', () => {
+        expect(footer.find('.Footer-title').text()).toEqual('Platzi Store')
+    })
 })
