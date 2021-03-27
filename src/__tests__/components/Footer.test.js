0167fa94228f3ebaee4ceb31be5eb456a66a3323
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-03-27 17:18:16 -0300

    Footer Snapshot

--- src/__tests__/components/Footer.test.js
+++ src/__tests__/components/Footer.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
-    const footer = mount(<Footer />);
-    test('component rendering', () => {
-        expect(footer.length).toEqual(1);
-    });
-    test('title rendering', () => {
-        expect(footer.find('.Footer-title').text()).toEqual('Platzi Store')
-    })
-})
+  const footer = mount(<Footer />);
+  test('component rendering', () => {
+    expect(footer.length).toEqual(1);
+  });
+  test('title rendering', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer Snapshot', () => {
+  test('check the UI of the Foother component', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
+  });
+});
